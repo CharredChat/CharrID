4d9a70bdd6fd79de56073a6de8798fbd979907cc
endercass <casimirrichardson123@gmail.com>
2023-04-25 20:44:41 -0400

    Added base struct for CharrID

--- charrid.go
+++ charrid.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// Bit lengths for ID components
+const (
+	LenTime = 48
+	LenProc = 8
+	LenIncr = 8
+)
+
+// Actual struct for IDs
+type CharrID struct {
+	Timestamp uint64
+	Proc      uint8
+	Incr      uint8
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello, World! %s", CharrID{18446744073709551615, 64, 40})
 }
